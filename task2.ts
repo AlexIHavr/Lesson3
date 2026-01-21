da579ec2bd0ef1e27ac456471c73d6739c0b4b92
DianaBych <dianavika18@gmail.com>
2026-01-21 21:57:15 +0300

    feat:resolve task2

--- task2.ts
+++ task2.ts
@@ -1,9 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const startNum: number = 1;
 const endNum: number = 20;
 
-const result: string = "0";
-
+let result: string = "0";
+let i = startNum;
 //Написать программу, которая выводит сумму всех чисел от 1 до 20
+while (i<= endNum){
+    result = String(+result + i);
+    i++;
+}
 
 console.log(result);
+
