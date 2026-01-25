c862a6ea3377df1182ec331d7beef21fde5ce10a
DianaBych <dianavika18@gmail.com>
2026-01-25 23:03:52 +0300

    refactor: add vars

--- task1.ts
+++ task1.ts
@@ -1,7 +1,9 @@
 const age = 23;
 const ageInDays = age * 365;
 const ageInHours = ageInDays * 24;
-const ageInMinutes = ageInHours *60;
+const ageInMinutes = ageInHours * 60;
+const ageInSeconds = ageInMinutes * 60;
+-
 //добавить минуты
 
-console.log(`Мне ${ageInDays} дней ${ageInHours} часов и ${ageInMinutes} минут`);
+console.log(`Мне ${ageInDays} дней ${ageInHours} часов и ${ageInSeconds} секунд`);
